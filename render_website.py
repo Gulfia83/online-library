99e2a0f59a57af4d6600ed8ee94c1820621460b7
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-08-06 10:06:11 +0300

    Add global costants

--- render_website.py
+++ render_website.py
@@ -4,22 +4,28 @@ from livereload import Server
 from more_itertools import chunked
 import os
 
+
+BOOKS_ON_PAGE = 20
+NUMBER_OF_COLUMNS = 2
+
+
 def on_reload():
+    path = os.getenv('PATH_TO_DESCRIPTIONS', default='attachments/books_descriptions.json')
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
     template = env.get_template('template.html')
 
-    with open('attachments/books_descriptions.json', 'r', encoding="utf8") as my_file:
+    with open(path, 'r', encoding="utf8") as my_file:
         books_descriptions_json = json.load(my_file)
 
-    books_descriptions = list(chunked(books_descriptions_json, 20))
+    books_descriptions = list(chunked(books_descriptions_json, BOOKS_ON_PAGE))
     pages_amount = len(books_descriptions)
     for page_number, books_on_page in enumerate(books_descriptions, 1):
 
         rendered_page = template.render(
-            books_on_page=list(chunked(books_on_page, 2)),
+            books_on_page=list(chunked(books_on_page, NUMBER_OF_COLUMNS)),
             pages_amount=pages_amount,
             page_number=page_number
         )
